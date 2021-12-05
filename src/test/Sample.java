717f83326bd24faf1e7b5379b9dc71b79f98aa67
kubonagi <nagi3ringo@gmail.com>
2021-12-05 22:58:13 +0900

    12.5 test.Sample

---
+++ src/test/Sample.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Sample {
+    static int num = 23;
+
+    public static void main(String[] args) {
+        int num = Sample.num;
+        System.out.println(num);
+    }
+}
